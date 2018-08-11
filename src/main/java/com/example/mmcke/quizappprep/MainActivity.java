81a966dc7d593df31cb6b719015d28bdcecc9264
Computer Instruction Team at Central <click1917@gmail.com>
2018-08-11 11:56:45 -0400

    Add files via upload
    Changes for Part 2

---
+++ src/main/java/com/example/mmcke/quizappprep/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.mmcke.quizappprep;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    //Score related variables
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void onSubmit(View v){
+        Toast.makeText(getApplicationContext(), "Hello World!", Toast.LENGTH_LONG).show();
+    }
+
+}
